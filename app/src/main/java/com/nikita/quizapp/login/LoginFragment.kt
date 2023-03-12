1931a738254c48b80d5ead6ea97dd4c2a2bfb29a
Bogdan <bogdanvolkov4585@gmail.com>
2023-03-12 15:32:33 +0600

    Pauk_Shnuk
    Pauk_Shnuk

--- app/src/main/java/com/nikita/quizapp/login/LoginFragment.kt
+++ app/src/main/java/com/nikita/quizapp/login/LoginFragment.kt
@@ -5,10 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.nikita.quizapp.R
 
 class LoginFragment: Fragment() {
+
+    private var userName: String=""
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -19,9 +24,27 @@ class LoginFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
          val button = view.findViewById<Button>(R.id.buttonLogin)
+        checkButtonEnabled()
+
          button.setOnClickListener {
-             print("hellow")
+            val parrentConrttoller = findNavController()
+             parrentConrttoller.navigate(R.id.action_loginFragment_to_quizFragment)
          }
+
+
+        val usertextFeild = view.findViewById<TextView>(R.id.userName_TextFeild)
+        usertextFeild.doOnTextChanged { text, start, before, count ->
+            userName = text.toString()
+           checkButtonEnabled()
+        }
+
     }
+
+    private fun checkButtonEnabled(){
+        val button = view?.findViewById<Button>(R.id.buttonLogin)
+        button?.isEnabled = !userName.isEmpty()
+    }
+
 }
